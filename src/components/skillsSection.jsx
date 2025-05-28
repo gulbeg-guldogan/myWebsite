d3243bfe365bd9bfdc0b57373bf7b0caf771e49b
gulbeg-guldogan <gulbeg.guldogan@gmail.com>
2025-05-28 10:37:40 +0300

    step 5.

--- src/components/skillsSection.jsx
+++ src/components/skillsSection.jsx
@@ -5,27 +5,28 @@ export default function SkillsSection() {
   const data = langData.skillsSection;
 
   return (
-    <section className="skill-section">
-      <div className="skill-container">
-        <p className="skill-title">
-          {data.title}
-        </p>
+   <section className="skill-section">
+    <div className="skill-container">
+      <p className="skill-title">
+        {data.title}
+      </p>
 
-        <div className="skill-grid">
-          {data.skills.map((skills) => (
-            <div className="skill-item" key={skills.name}>
-              <img
-                className="skill-icon"
-                src={skills.icon}
-                alt={skills.name}
-              />
-              <p className="skill-name">
-                {skills.name}
-              </p>
-            </div>
-          ))}
-        </div>
+      <div className="skill-grid">
+        {data.skills.map((skills) => (
+          <div className="skill-item" key={skills.name}>
+            <img
+              className="skill-icon"
+              src={skills.icon}
+              alt={skills.name}
+            />
+            <p className="skill-name">
+              {skills.name}
+            </p>
+          </div>
+        ))}
       </div>
-    </section>
+    </div>
+  </section>
+
   );
 }
